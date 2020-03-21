5b565704daca4014702c98d146650af9319b164a
ali <alighahremani.developer@gmail.com>
2020-03-21 23:18:46 +0430

    create lozenge with stars

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+import com.sun.deploy.util.StringUtils;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int counter = 1;
+        int SpaceCounter = 3;
+        for (int i=0;i<4;i++){
+            System.out.print(RepeatStr(" ",SpaceCounter));
+            System.out.print(RepeatStr("*",counter));
+            counter +=2;
+            SpaceCounter--;
+            System.out.println("");
+        }
+
+        counter = 5;
+        SpaceCounter = 1;
+        for (int i=counter;i>0;i--){
+
+            System.out.print(RepeatStr(" ",SpaceCounter));
+            System.out.print(RepeatStr("*",counter));
+            counter -=2;
+            SpaceCounter++;
+            System.out.println("");
+        }
+    }
+
+    static String RepeatStr(String str,int counter){
+        String result = "";
+        for (int i=0;i<counter;i++){
+            result += str;
+        }
+        return result;
+    }
+}
